3f053e5c8668069c4359f1c70767143f60fc4344
eaguidigodo <eaguidigodo@gmail.com>
2022-09-02 10:45:37 +0100

    merged all files

--- vhosts.sh
+++ vhosts.sh
@@ -25,7 +25,20 @@ while true; do
     select host_type in HTTP HTTPS QUITTER; do
         case $host_type in
             HTTP)
-                ./http.sh
+                [ -d $CONFDIR ] || mkdir -p $CONFDIR
+                cat $TEMPLATE > $CONFDIR/$domain_name.conf
+                #Générer le template toi même
+                sed -i "s/nom_de_domaine/$domain_name/g" $CONFFILE
+                sed -i "s/racine_projet/$WEBDIR/g" $CONFFILE
+                echo "Nous venons de configurer le fichier, nous allons à
+                présent activer le site et redémarrer le serveur"
+                a2ensite $domain_name.conf
+                a2dissite 000-default.conf
+                systemctl restart apache2
+                #Vérifier après l'état des services afin de pouvoir avertir
+                #le user en cas de problème...
+                echo "Configuration des droits des dossiers..."
+                chmod -R 755 /var/www
                 echo "Et Voilà votre host est configuré en HTTP.
                       Vous pouvez configurer votre fichier host sur votre
                 machine locale. Voulez vous lui ajouter un certificat?"
@@ -46,8 +59,45 @@ while true; do
             ;;
             
             HTTPS)
-                ./http.sh
-                ./https.sh
+                [ -d $CONFDIR ] || mkdir -p $CONFDIR
+                cat $TEMPLATE > $CONFDIR/$domain_name.conf
+                #Générer le template toi même
+                sed -i "s/nom_de_domaine/$domain_name/g" $CONFFILE
+                sed -i "s/racine_projet/$WEBDIR/g" $CONFFILE
+                echo "Nous venons de configurer le fichier, nous allons à
+                présent activer le site et redémarrer le serveur"
+                a2ensite $domain_name.conf
+                a2dissite 000-default.conf
+                systemctl restart apache2
+                #Vérifier après l'état des services afin de pouvoir avertir
+                #le user en cas de problème...
+                echo "Configuration des droits des dossiers..."
+                chmod -R 755 /var/www
+                echo "Super on va commencer par activer le module
+                ssl et redemarrer le serveur:.."
+                a2enmod ssl
+                systemctl restart apache2
+                echo "Nous allons à présent générer un certificat
+                TLS...."
+                openssl req -x509 -nodes -days 365 -newkey\
+                rsa:2048 -keyout\
+                /etc/ssl/private/apache-selfsigned.key\
+                -out /etc/ssl/certs/apache-selfsigned.crt
+                echo "Aller, il est temps de configurer apache pour
+                qu'il utilise TLS"
+                sed s "/80/443" $CONFFILE
+                sed -i "/<\/VirtualHost>/i SSLEngine on\
+        \n SSLCertificateFile /etc/ssl/certs\
+        /apache-selfsigned.crt\
+        \n  SSLCertificateKeyFile\
+        /etc/ssl/private/\
+                apache-selfsigned.key" $CONFFILE
+                echo "Nous allons à présent rédiriger tous les
+                traffics http en https...."
+                sed -i "/<\/VirtualHost>/a <VirtualHost *:80>\
+        \n ServerName $domain_name\
+        \n Redirect / https://$domain_name \
+                \n <\/VirtualHost>" $CONFFILE
                 break
             ;;
             QUITTER)
