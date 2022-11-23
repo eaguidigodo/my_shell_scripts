d8e46b2fe770d1522e0de9207a35c2269fd2acef
Enselme <eaguidigodo@gmail.com>
2022-11-23 17:56:10 +0100

    Correct http

--- automate_virtualhosts_creation/http.sh
+++ automate_virtualhosts_creation/http.sh
@@ -8,7 +8,7 @@ cat $TEMPLATE > $CONFDIR/$CONFFILE.conf
 echo "On dirait que je me bloque ici"
 #Générer le template toi même
 
-sed  "s@nom_de_domaine@$CONFFILE.local/g" $CONFFILE.conf
+sed  "s@nom_de_domaine@$CONFFILE.local@g" $CONFFILE.conf
 sed  "s@racine_projet@$WEBDIR/$project_path@g" $CONFFILE.conf
 echo "Nous venons de configurer le fichier, nous allons à
 présent activer le site et redémarrer le serveur"
