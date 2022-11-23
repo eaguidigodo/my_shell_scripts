31c813a834fa5c1e8adb64f083ac33b5aa7074af
Enselme <eaguidigodo@gmail.com>
2022-11-23 18:35:39 +0100

    Correct http

--- automate_virtualhosts_creation/http.sh
+++ automate_virtualhosts_creation/http.sh
@@ -5,6 +5,7 @@
 echo "Yo j'ai été appelé, me voici"
 echo "Voici la variable template: $TEMPLATE"
 cat $TEMPLATE > $CONFDIR/$CONFFILE.conf
+echo "Je suis le conffile: $CONFFILE, $CONFFILE.conf"
 echo "On dirait que je me bloque ici"
 #Générer le template toi même
 
